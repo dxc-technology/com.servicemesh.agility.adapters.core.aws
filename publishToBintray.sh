65cfca65a763472186fd4dde60066354f9042c2d
Dustin Behnke <dustin.behnke@servicemesh.com>
2016-06-14 16:13:57 -0500

    Update build to publish to bintray instead of artifactory

---
+++ publishToBintray.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Uploading artifacts to bintray at https://bintray.com/csc/opensource/com.servicemesh.agility.adapters.core.aws"
+
+cd dist
+
+shopt -s nullglob
+for file in *;
+do
+   curl -T $file -u$2:$3 "https://api.bintray.com/content/csc/opensource/com.servicemesh.agility.adapters.core.aws/$1/$file?publish=1&override=1"
+done
+
+
