2d2dd36ee9adfaf7f5850a6f3768b15397aa3958
Dustin Behnke <dustin.behnke@servicemesh.com>
2016-06-15 09:35:21 -0500

    Fix publish script to save to version

--- publishToBintray.sh
+++ publishToBintray.sh
@@ -7,7 +7,7 @@ cd dist
 shopt -s nullglob
 for file in *;
 do
-   curl -T $file -u$2:$3 "https://api.bintray.com/content/csc/opensource/com.servicemesh.agility.adapters.core.aws/$1/$file?publish=1&override=1"
+   curl -T $file -u$2:$3 "https://api.bintray.com/content/csc/opensource/com.servicemesh.agility.adapters.core.aws/$1/$file;publish=1;override=1;bt_package=com.servicemesh.agility.adapters.core.aws;bt_version=$1"
 done
 
 
